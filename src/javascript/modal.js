76789d98004b1d35ed2132a957dc2a7e49cf1031
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-10-29 15:46:53 +0100

    #2 Fix a user-selection bug when the modal box lunched

--- src/javascript/modal.js
+++ src/javascript/modal.js
@@ -4,7 +4,8 @@ const blurBg = document.getElementById("blur-bg");
 
 closeIcon.addEventListener("click", () => {
     modal.style.display = "none";
-    blurBg.style.filter = "blur(0px)";  
+    blurBg.style.filter = "blur(0px)";
+    blurBg.style.userSelect = "";  
 });
 
 const addBtn = document.getElementById("add-btn");
@@ -12,4 +13,5 @@ const addBtn = document.getElementById("add-btn");
 addBtn.addEventListener("click", () => {
     modal.style.display = "block";
     blurBg.style.filter = "blur(2px)"; 
+    blurBg.style.userSelect = "none";
 })
