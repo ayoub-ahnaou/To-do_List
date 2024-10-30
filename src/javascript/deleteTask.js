fbadc3aee07d68a894686100de3840c8c380e54f
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-10-30 17:29:30 +0100

    #7 Handle the changement of count tasks after delete

--- src/javascript/deleteTask.js
+++ src/javascript/deleteTask.js
@@ -1,4 +1,11 @@
 
-function handleDeleteTask(event) {
-    event.target.parentElement.parentElement.parentElement.remove();
+function handleDeleteTask(event, statut) {
+  if(statut == "to-do")
+    document.getElementById("countRedTasks").innerHTML = "To-Do: " + --countRedTasks;
+  if(statut == "doing")
+    document.getElementById("countOrangeTasks").innerHTML = "In progress: " + --countOrangesTasks;
+  if(statut == "done")
+    document.getElementById("countGreenTasks").innerHTML = "Done: " + --countGreensTasks;
+
+  event.target.parentElement.parentElement.parentElement.remove();
 }
