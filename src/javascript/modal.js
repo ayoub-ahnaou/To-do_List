a1076bba9c34092b0cd3b1fb39029f23044c465b
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-10-31 23:28:32 +0100

    #3 Handle the messages error of the two inputs, priorety and date in addTask function

--- src/javascript/modal.js
+++ src/javascript/modal.js
@@ -5,23 +5,25 @@ const blurBg = document.getElementById("blur-bg");
 closeIcon.addEventListener("click", () => {
   modal.style.display = "none";
   blurBg.style.filter = "blur(0px)";
-  blurBg.style.userSelect = "";
 
   // make all inputs empty after closing the modal window
   document.getElementById("title").value = "";
   document.getElementById("description").value = "";
   document.getElementById("statut").value = "";
+  document.getElementById("deadline").value = "";
+  document.getElementById("priorety").value = "";
 
   // hide the error messages
   document.querySelector("#err-mssg-title").style.display = "none";
   document.querySelector("#err-mssg-desc").style.display = "none";
   document.querySelector("#err-mssg-statut").style.display = "none";
+  document.querySelector("#err-mssg-deadline").style.display = "none";
+  document.querySelector("#err-mssg-priorety").style.display = "none";
 });
 
 const addBtn = document.getElementById("add-btn");
 
 addBtn.addEventListener("click", () => {
   modal.style.display = "block";
-  blurBg.style.filter = "blur(2px)";
-  blurBg.style.userSelect = "none";
+  blurBg.style.filter = "blur(8px)";
 });
